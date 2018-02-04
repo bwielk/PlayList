6d83b5c4f17ddac11912adddc12e74a4e540e2c9
bwielk <bwielk@gmail.com>
2018-02-04 19:23:17 +0100

    Song class's been created

--- tests/playlist/SongTest.java
+++ tests/playlist/SongTest.java
@@ -5,20 +5,29 @@ import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Test;
 
-public class SongTest extends Song {
+public class SongTest{
 
 	private Song song1;
 	private Song song2;
 	
 	@Before
 	public void before(){
-		song1 = new Song();
-		song2 = new Song();
+		song1 = new Song("Amy Winehouse", "Back to black", "Rehab");
+		song2 = new Song("Amy Winehouse", "Me and Mr Jones",  "Rehab");
 	}
 	
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void song1HasSpec(){
+		assertEquals("Back to black", song1.getTitle());
+		assertEquals("Amy Winehouse", song1.getArtist());
+		assertEquals("Rehab", song1.getAlbum());
+	}
+	
+	@Test
+	public void song2HasSpec(){
+		assertEquals("Me and Mr Jones", song2.getTitle());
+		assertEquals("Amy Winehouse", song2.getArtist());
+		assertEquals("Rehab", song2.getAlbum());
 	}
 
 }
