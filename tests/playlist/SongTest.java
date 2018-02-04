b5ecd2c3bcb6f57f58171b60080a52cf847e2c6e
bwielk <bwielk@gmail.com>
2018-02-04 19:13:51 +0100

    Initial commit with Song class

---
+++ tests/playlist/SongTest.java
@@ -0,0 +1,24 @@
+package playlist;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class SongTest extends Song {
+
+	private Song song1;
+	private Song song2;
+	
+	@Before
+	public void before(){
+		song1 = new Song();
+		song2 = new Song();
+	}
+	
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
