c41a9f85bf0e6dd4f9210aba1b1c3e10a0e0c8eb
bwielk <bwielk@gmail.com>
2018-02-10 14:02:11 +0100

    PlaylistTest setup

---
+++ tests/playlist/PlaylistTest.java
@@ -0,0 +1,50 @@
+package playlist;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class PlaylistTest {
+
+	private Player player;
+	private Song song1;
+	private Song song2;
+	private Song song3;
+	private Song song4;
+	private Song song5;
+	private Album album1;
+	private Album album2;
+	
+	@Before
+	public void before(){
+		player = new Player();
+		player.createPlaylist("Rock");
+		player.createPlaylist("Soul");
+		song1 = new Song("The Cranberries", "Animal Instinct", "Bury The Hatchet");
+		song2 = new Song("The Cranberries", "Fee Fi Fo", "Bury The Hatchet");
+		song3 = new Song("The Cranberries", "Loud and Clear", "Bury The Hatchet");
+		song4 = new Song("Amy Winehouse", "Back to Black", "Back to Black");
+		song5 = new Song("The Velvet Underground", "All Tomorrow's Parties", "The Velvet Underground");
+		album1 = new Album(false, "The Cranberries", "Bury The Hatchet");
+		album2 = new Album(true, "Various Artist", "Various Songs");
+		album1.addSong(song1);
+		album1.addSong(song2);
+		album1.addSong(song3);
+		album2.addSong(song4);
+		album2.addSong(song5);
+	}
+	
+	@Test
+	public void playlistIsStoreInPlayer(){
+		assertEquals(2, player.getPlaylists().size());
+	}
+	
+	@Test
+	public void playerAddsANewSongFromAnExistingAlbumToAPlaylist(){
+		assertEquals(true, player.addSongToPlaylist(song1, "Rock"));
+		assertEquals(true, player.addSongToPlaylist(song2, "Rock"));
+		assertEquals(true, player.addSongToPlaylist(song5, "Rock"));
+		assertEquals(3, player.getPlaylistByName("Rock").size());
+	}
+}
